8fb1c8f9315320f1b4964529bdfb726907e81246
unknown <thakdao@gmail.com>
2018-09-12 17:05:23 +0900

    lean array.map

---
+++ map.js
@@ -0,0 +1,13 @@
+/**
+ * 1. Viết hàm tripple nhận vào 1 số và trả về số đó nhân 3
+ * 2. Sử dụng map method và hàm tripple để nhân 3 các phần tử trong mảng dưới đây
+ */
+// Khai báo tripple function ở đây
+
+
+var numbers = [10, 5, 8];
+numbers.map(function tripple(num){{
+  return num*3;
+}
+})
+// Gợi ý: numbers.map... (tự viết tiếp)
