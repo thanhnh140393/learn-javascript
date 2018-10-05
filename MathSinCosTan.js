f28b960599372edc124d088356d63934018dd486
thanhnh140393 <42526138+thanhnh140393@users.noreply.github.com>
2018-10-05 09:42:36 +0900

    lean Math Object

---
+++ MathSinCosTan.js
@@ -0,0 +1,17 @@
+/* 
+  Viết hàm trigonim để in ra 3 giá trị sin cos tan của một số nhập vào
+*/
+
+function trigonim(number) {
+  var sin =Math.sin(number);
+  var cos =Math.cos(number);
+  var tan =Math.tan(number);
+    console.log(sin);
+    console.log(cos);
+    console.log(tan);
+}
+
+trigonim(45);
+// Sin(45) = 0.850
+// Cos(45) = 0.525
+// Tan(45) = 1.619
